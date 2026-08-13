a15799586552f5a33b53716e35c0238235b10010
adambernstein1492 <adambernstein1492@gmail.com>
2026-08-13 01:31:28 -0700

    Update models.py

--- models.py
+++ models.py
@@ -32,6 +32,7 @@ def Xu2026(weight, age, creatinine):
     age_day = age
     age_year = age / 365.25
     Age_Factor = age_day ** 3.4 / (age_day ** 3.4 + 47.7 ** 3.4)
+    creatinine_umL = creatinine * 88.42
     StandardizedCreatinine = 2.37330 - 12.91367 * np.log(age_year) + 23.93581 * age_year ** 0.5
     Fscr = np.exp(-0.649 * (creatinine - StandardizedCreatinine))
     CrCl = 7.56 * (weight / 70) ** 0.75 * Age_Factor * Fscr
