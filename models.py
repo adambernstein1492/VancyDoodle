f10d98891e089884f6fcfc7fb7716800a132c7c2
adambernstein1492 <adambernstein1492@gmail.com>
2026-08-13 01:36:46 -0700

    Update models.py

--- models.py
+++ models.py
@@ -31,6 +31,7 @@ def Smit2021(weight, height, creatinine):
 def Xu2026(weight, age, creatinine):
     age_day = age
     age_year = age / 365.25
+    print(age)
     Age_Factor = age_day ** 3.4 / (age_day ** 3.4 + 47.7 ** 3.4)
     creatinine_umL = creatinine * 88.42
     StandardizedCreatinine = 2.37330 - 12.91367 * np.log(age_year) + 23.93581 * age_year ** 0.5
